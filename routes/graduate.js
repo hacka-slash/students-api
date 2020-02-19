6331bb77c45c58e25e39c58715be28b92e7570ad
hacka-slash <placido.hoff@gmail.com>
2020-02-18 23:32:42 -0500

    Simply Displaying the Names Saved to my DB

--- routes/graduate.js
+++ routes/graduate.js
@@ -2,9 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Graduate = require('../models/Graduate')
 
-router.get('/', (req, res) => {
-    res.send('This is the graduates Route');
+router.get('/',async (req, res) => {
+    // res.send('This is the graduates Route');
+    // console.log("Access to graduate route")
+    try{
+        const graduates = await Graduate.find();
+        res.json(graduates);
+        console.log(graduates);
+    }catch(err){
+        res.json({message: err});
+    }
 })
+
 
 router.post('/', async (req, res) => {
     const graduate = new Graduate(req.body);
